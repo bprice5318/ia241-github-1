f3aee16c059e9def39cf3ea41f501919b595b183
EC2 Default User <ec2-user@ip-172-31-52-162.ec2.internal>
2022-01-31 19:48:46 +0000

    lab2

---
+++ lab2.py
@@ -0,0 +1,33 @@
+'''
+lab2
+
+'''
+my_name = "Tom"
+
+print (my_name.upper())
+
+
+
+my_id = 123
+
+print (my_id)
+
+
+my_id = your_id = 123
+
+
+my_id_str = "123"
+
+print (my_id_str)
+
+#print (my_name + my_id)
+
+print (my_name + my_id_str)
+
+print (my_name * 3) 
+
+first = "hello, world. This is my first python string."
+
+print (first.split("."))
+
+
---
+++ lec2.py
@@ -0,0 +1,21 @@
+"""
+lec 2
+this is a regional comment
+
+"""
+
+#print("hello world") #this is a single line comment 
+
+print(type(123))
+
+print (type (123.))
+
+print (type ("123"))
+
+my_int = 4 
+
+print (my_int)
+
+my_string = "this is a string "
+
+print (my_string.strip())
